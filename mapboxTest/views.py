0b0e1c30c2a69c502093c699fbb3aba996e5844c
hansujung <zqx1@naver.com>
2021-04-02 15:57:47 +0900

    Update views.py

--- mapboxTest/views.py
+++ mapboxTest/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def test_map(request):
-    mapboxToken = "pk.eyJ1IjoiaGFzc3p6IiwiYSI6ImNqbGdibnB0czE1dnUza3BhM3ZiZGE1MmMifQ.PdUA8uR-a7BApNBugthSQA"
+    mapboxToken = Token
     content = { "mapboxToken":mapboxToken }
     return render(request, 'mapboxTest/test_map.html', content)
